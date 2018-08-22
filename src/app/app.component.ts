9cca12bf007546c6fec1b84e97b63602887c70d6
vikasnirwal73 <vikasnirwal73@gmail.com>
2018-08-22 22:43:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// const currData = require('./curr_data.json');
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  currData = require('./curr_data.json');
+  arrData = Object.values(this.currData);
+  constructor(private http: HttpClient) {}
+  convertedData: number;
+  ngOnInit() {
+  }
+  onClick(value, con1, con2) {
+    if(value.value && con1.value && con2.value){}
+    const conv_str = con1.value + '_' + con2.value;
+    this.http.get(`http://free.currencyconverterapi.com/api/v5/convert?q=${conv_str}&compact=y`)
+    .subscribe((data) => {
+      //console.log(data);
+      this.convertedData = data[conv_str].val * value.value;
+    }, (error) => {
+      console.log(error);
+    });
+    }
+  }
+//http://free.currencyconverterapi.com/api/v5/convert?q=USD_INR&compact=y
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
