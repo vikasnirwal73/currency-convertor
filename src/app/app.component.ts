0002275bdac957b52d7dc6e6f43ba04c4e6fbc55
vikasnirwal73 <vikasnirwal73@gmail.com>
2018-08-26 10:54:28 +0530

    Add form validation, toolbar and some other small tweeks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,90 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-// const currData = require('./curr_data.json');
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
-  currData = require('./curr_data.json');
-  arrData = Object.values(this.currData);
-  constructor(private http: HttpClient) {}
-  convertedData: number;
-  ngOnInit() {
-  }
-  onClick(value, con1, con2) {
-    if(value.value && con1.value && con2.value){}
-    const conv_str = con1.value + '_' + con2.value;
-    this.http.get(`http://free.currencyconverterapi.com/api/v5/convert?q=${conv_str}&compact=y`)
-    .subscribe((data) => {
-      //console.log(data);
-      this.convertedData = data[conv_str].val * value.value;
-    }, (error) => {
-      console.log(error);
+
+
+  countriesData: any[];
+  convertedData: any;
+  loading = false;
+  clean = true;
+  messageToShow = '';
+  form: FormGroup;
+
+  constructor(
+    private http: HttpClient,
+    private fb: FormBuilder,
+    public snackBar: MatSnackBar
+  ) {
+    this.form = this.fb.group({
+      amount: ['', Validators.required],
+      referenceCountry: ['', Validators.required],
+      targetCountry: ['', Validators.required]
     });
+  }
+
+  ngOnInit() {
+    this.http.get('./assets/data.json').subscribe(
+      data => {
+        console.log(data);
+        this.countriesData = Object.values(data);
+      },
+      error => {
+        console.log(error);
+        this.snackBar.open('Somthing went wrong :(', 'OK', {
+          duration: 3000
+        });
+      }
+    );
+  }
+
+  onSubmit() {
+    const amount = this.form.value.amount;
+    const referenceCountry = this.form.value.referenceCountry;
+    const targetCountry = this.form.value.targetCountry;
+    console.log(this.form);
+    if (this.form.valid) {
+      console.log(amount, referenceCountry, targetCountry);
+      const conv_str = referenceCountry + '_' + targetCountry;
+      this.loading = true;
+      this.clean = false;
+      this.messageToShow = '';
+      this.http
+        .get(
+          `http://free.currencyconverterapi.com/api/v5/convert?q=${conv_str}&compact=y`
+        )
+        .subscribe(
+          data => {
+            this.convertedData = data[conv_str].val * amount;
+            const currencyFrom = this.countriesData.filter( key => key.code === referenceCountry );
+            const currencyTo = this.countriesData.filter( key => key.code === targetCountry );
+            console.log(currencyFrom);
+            this.messageToShow = `${ amount }
+               ${ amount === 1 ? currencyFrom[0].name : currencyFrom[0].name_plural }
+               <br /> = <span class="result-amount">${ this.convertedData }
+               ${ this.convertedData === 1 ? currencyTo[0].name : currencyTo[0].name_plural }</span> `;
+            this.loading = false;
+          },
+          error => {
+            this.loading = false;
+            this.messageToShow = 'Somthing went wron with the API server';
+            this.snackBar.open('Could not connect to the API server.', 'OK', {
+              duration: 2500
+            });
+          }
+        );
+    } else {
+      this.snackBar.open('Please fill all fields.', 'ok', {
+        duration: 2000
+      });
     }
   }
-//http://free.currencyconverterapi.com/api/v5/convert?q=USD_INR&compact=y
 }
-
-
-
+// http://free.currencyconverterapi.com/api/v5/convert?q=USD_INR&compact=y
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatDividerModule} from '@angular/material/divider';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -20,9 +29,18 @@ import {MatInputModule} from '@angular/material/input';
     MatFormFieldModule,
     MatButtonModule,
     MatInputModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatDividerModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
